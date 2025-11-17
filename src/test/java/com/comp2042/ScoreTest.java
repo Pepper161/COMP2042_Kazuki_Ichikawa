fde37062b1fffa3c09aecb82a2433daa7c3ccbff
Pepper161 <ichikawaforbusiness@gmail.com>
2025-11-17 11:42:54 +0800

    test: expand board and score coverage (close #8)

--- src/test/java/com/comp2042/ScoreTest.java
+++ src/test/java/com/comp2042/ScoreTest.java
@@ -43,4 +43,29 @@ class ScoreTest {
         score.handleLineClear(new LineClearStats(LineClearType.T_SPIN_DOUBLE, 2));
         assertEquals(1200, score.scoreProperty().get());
     }
+
+    @Test
+    void backToBackAwardsFiftyPercentBonus() {
+        Score score = new Score();
+        score.handleLineClear(new LineClearStats(LineClearType.TETRIS, 4));
+        assertEquals(800, score.scoreProperty().get());
+
+        score.handleLineClear(new LineClearStats(LineClearType.TETRIS, 4));
+
+        assertEquals(2050, score.scoreProperty().get(),
+                "Second Tetris should include B2B bonus (+400) and combo bonus (+50)");
+    }
+
+    @Test
+    void resetClearsAllState() {
+        Score score = new Score();
+        score.handleLineClear(new LineClearStats(LineClearType.TRIPLE, 3));
+        score.handleLineClear(new LineClearStats(LineClearType.DOUBLE, 2));
+
+        score.reset();
+
+        assertEquals(0, score.scoreProperty().get());
+        assertEquals(0, score.comboProperty().get());
+        assertEquals(0, score.backToBackProperty().get());
+    }
 }
